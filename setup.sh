05fded0876523af003b15257d7bc45ab5c1604aa
Arijit Ory Sarcar <sarcar_a@yahoo.com>
2013-07-14 07:55:27 +0000

    Create repository for personal dotfiles and scriptable infrastructure

--- setup.sh
+++ setup.sh
@@ -34,10 +34,14 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
+if [ -d .env_custom/ ]; then
+    mv .env_custom .env_custom~
+fi
 git clone https://github.com/startup-class/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
+ln -sf dotfiles/.env_custom .
 
